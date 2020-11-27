959d9e61379aea3184ad2f8533b7a7949e873429
zahmis <meda829ratteru86@gmail.com>
2020-11-28 05:37:04 +0900

    add reactstrap

---
+++ src/HeaderNavbar.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const HeaderNavbar = (props) => {
+  return (
+        <div>hoge</div>
+  );
+};
+
+export default HeaderNavbar;
--- src/index.jsx
+++ src/index.jsx
@@ -1,13 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Blog from './Blog';
+import 'bootstrap/dist/css/bootstrap.css';
+// import HeaderNavbar from './HeaderNavbar';
 
 ReactDOM.render(
   <React.Fragment>
-    <div>
-      hoge
-      <Blog />
-    </div>
-    </React.Fragment>,
+   <nav class="navbar navbar-expand-lg navbar-light bg-info">
+     hoge
+    </nav>
+  </React.Fragment>,
   document.getElementById('app'),
 );
